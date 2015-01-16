a2c6204d1138f8d1a6a862526d8bea99e170d4d3
hawk0484 <hawk0484@hotmail.co.uk>
2015-01-16 00:17:09 +0000

    Beacse

--- src/backup/game/Entity.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-public class Entity {
-	protected Entity(int id){
-		Entitys[id]=this;
-	}
-	public static final Entity[] Entitys = new Entity[256];
-}
--- src/backup/game/Menu.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-public interface Menu {
-	public void render();
-	public void click(int button, int x, int y, int clickcount);
-	public String getState();
-	public void update();
-}
